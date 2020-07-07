045f2908625b374499f19184299b92867ce252d2
Andrew McCartney <acm9q@virginia.edu>
2020-07-07 17:38:34 -0400

    Create data_import.R

---
+++ data_import.R
@@ -0,0 +1,11 @@
+# tracking meta
+
+
+# run once: 
+devtools::install_github("tidyverse/googlesheets4")
+
+library(googlesheets4)
+
+
+tracking<-read_sheet("https://docs.google.com/spreadsheets/d/1bVys5zA0NMs7Hn88v1AS_vgyw4P_QKe28-OB_gQKzo4/edit#gid=0")
+

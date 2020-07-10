4bf2b04bb29268083c00c0a99f585b9e270fc1ed
Andrew McCartney <acm9q@virginia.edu>
2020-07-10 08:39:30 -0400

    Create initial_look.R

---
+++ initial_look.R
@@ -0,0 +1,8 @@
+source("") 
+
+tracking %>% 
+  mutate(N = as.numeric(gsub('\\D+','', `Number of Participants (N)`))) %>% 
+  mutate(K =  as.numeric(gsub('\\D+','', `Number of Participant Groups (if applicable) (K)`))) %>% 
+  mutate(D1 =  as.numeric(gsub('\\D+','', `Main Effect Size (D1)`))) %>% 
+  select(N,K, D1) %>% 
+  psych::describe(fast = TRUE)
